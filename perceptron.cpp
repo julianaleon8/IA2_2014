b1f73ddfeed5b3a9d0cc4ab4774c5075d29c0e5d
TroiKM <karentroiano2009@gmail.com>
2014-10-03 10:30:42 -0430

    Aprende, nunca termina de aprender pero aprende algo.

--- perceptron.cpp
+++ perceptron.cpp
@@ -4,53 +4,61 @@
  * 09-10203 Stefano De Colli 
  */
 
+using namespace std;
+
 #include <stdio.h>
 #include <iostream>
 
 
+int x1 [7] = {0,0,0,1,1,1,1};
+int x2 [7] = {0,0,1,0,1,0,1};
+int x3 [7] = {0,1,0,0,0,1,1};
 
-using namespace std;
+int desired [7] = {0,0,0,0,0,0,1};
 
-int x1 [6] = {0,0,0,1,1,1};
-int x2 [6] = {0,0,1,0,1,0};
-int x3 [6] = {0,1,0,0,0,1};
-int desired [6] = {0,0,0,0,0,0};
-int threshold = 0.5;
-int learning_rate = 0.1;
-int weights [3] = {0, 0, 0};
+float threshold = 0.5;
+float learning_rate = 0.5;
+float weights [3] = {0.0, 0.0, 0.0};
 
 
 int classification (int i ){
-		
-	return (x1[i]*desired[i]+ x2[i]*desired[i]+ x3[i]*desired[i]);	 
+    	
+	return (x1[i]*desired[i] + x2[i]*desired[i] + x3[i]*desired[i]);	 
 
 }
 
 
 int main (int argc, char ** argv ) {
 
-	while (1){
+	while (1)
+	{
 		int r = 0;
 		int count_error = 0;
-		int i = 0 ;
-		for ( i ; i < 7; i++){
-			cout << " primero " << weights[1] << weights[2]<< weights[3];
+        
+		for (int i = 0 ; i < 8; ++i)
+		{
+			cout << " weights: " << weights[1] << " " << weights[2] << " " << weights[3] << endl;
+			
 			if (classification (i) > threshold )
 				r = 0;
 			else 
 				r = 1;
+			
 			int error = desired[i] - r;
-			if (error != 0 ){
-				int j = 0 ; 
-				for (j ; j< 3; j++)
-					weights [j] = weights[j-1] + (learning_rate * error * x1[i]) +
-						(learning_rate * error * x2[i]) + (learning_rate * error * x3[i]);
+			
+			if (error != 0 ){ 
+				count_error++;
+				for (int j = 0; j < 3; ++j)
+				{    
+                    weights [j] += (learning_rate * error * x1[i]) +
+						(learning_rate * error * x2[i]) + (learning_rate * error * x3[i]);				
+				}
+                
 			}
 			
 		}
 		if 	(count_error == 0)
 			break;
-		
 	}
 	
 }
