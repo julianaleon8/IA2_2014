0152792b3bef92a888cbe78e3bbfaf08a0e77efa
Stefano De Colli <sdecolli@gmail.com>
2014-10-03 19:09:52 -0430

    ANDOROAND

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * 08-10608 Juliana Leon
  * 09-10855 Karen Troiano
- * 09-10203 Stefano De Colli 
+ * 09-10203 Stefano De Colli
  */
 
 using namespace std;
@@ -9,60 +9,56 @@ using namespace std;
 #include <stdio.h>
 #include <iostream>
 
+#define LEARNING_RATE	0.1f
+#define NUM_OF_X		3
+#define NUM_CASES		4
+#define MAX_ITER		150
 
-int x1 [7] = {0,0,0,1,1,1,1};
-int x2 [7] = {0,0,1,0,1,0,1};
-int x3 [7] = {0,1,0,0,0,1,1};
+int x1 [4] = {1, 1, 1, 1};
+int x2 [4] = {0, 1, 0, 1};
+int x3 [4] = {0, 0, 1, 1};
 
-int desired [7] = {0,0,0,0,0,0,1};
+int desired [4] = {0, 0, 0, 1};
 
-float threshold = 0.5;
-float learning_rate = 0.1;
 float weights [3] = {0.0, 0.0, 0.0};
-int inter =50;
-float classification (int i ){
-    
-	return (x1[i]*weights[0] + x2[i]*weights[1] + x3[i]*weights[2]);	 
 
+float classification(int i) {
+	return (x1[i] * weights[0] + x2[i] * weights[1] + x3[i] * weights[2]);
 }
 
-
-int main ( void) {
+int main(void) {
 	int count = 0;
-	while (count < inter){
+	while(count < MAX_ITER) {
 		int r = 0;
 		count ++;
 		int count_error = 0;
-        
-		for (int i = 0 ; i < 8; ++i)
-		{
+
+		for(int i = 0 ; i < 4; ++i) {
 			cout << " weights: " << weights[0] << " " << weights[1] << " " << weights[2] << endl;
-			float result = classification (i); 
-		
-			if ( result > threshold ){
-				
-				r = 0;
-			}
-			else 
+			float result = classification(i);
+
+			if(result > 0)
+
+				r = -1;
+			else
 				r = 1;
-			
+
 			int error = desired[i] - r;
-			
-			if (error != 0 ){ 
+
+			if(error != 0) {
 				count_error++;
-				for (int j = 0; j < 3; ++j)
-				{
-					int entrada [3] = {x1[i],x2[i],x3[i]}; 
-                    weights [j] += (learning_rate * error * entrada[j]);
-						cout << " entrada" << entrada[j];
+				for(int j = 0; j < 3; ++j) {
+					int entrada [3] = {x1[i], x2[i], x3[i]};
+					weights [j] += (LEARNING_RATE * error * entrada[j]);
+					cout << " entrada" << entrada[j];
 
 				}
-                
+
 			}
-			
+
 		}
-		if 	(count_error == 0)
+		if(count_error == 0)
 			return 0;
 	}
-	
+
 }
