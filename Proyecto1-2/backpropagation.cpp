29549fb84577cb4af88e47e5b4da2340ec597562
troikm <karentroiano2009@gmail.com>
2014-10-16 21:33:43 -0430

    comida

--- Proyecto1-2/backpropagation.cpp
+++ Proyecto1-2/backpropagation.cpp
@@ -1,14 +1,40 @@
 #include "backpropagation.h"
 
+using namespace std;
+
+inline double Backpropagation::dot_product(vector<float> inputCase,float *w) 
+{
+	double res = 0;
+	for (int i = 0; i < 3; ++i)
+		res += inputCase[i] * w[i];
+	return res;
+}
+
+
+inline double Backpropagation::sigma(double x) 
+{
+	return 1 / (1 + exp(-x));
+}
+
+inline double Backpropagation::devSigma(double x)
+{
+	return x * (1 - x);
+}
+
 void Backpropagation::train(vector< vector<float> > examples, float learning_rate)
 {
 	
+	vector<double> in; 
+	double ini = 0.0;
 	for(;;)
 	{
-		
 		for (std::vector< vector<float> >::iterator it = examples.begin() ; it != examples.end(); ++it)
 		{
+			ini = dot_product(*it , weights[0][0]);
+			in.push_back(sigma(ini));
 			
+			ini = dot_product(*it, weights[0][1]);
+			in.push_back(sigma(ini));
 		}
 	}
 }
--- Proyecto1-2/backpropagation.h
+++ Proyecto1-2/backpropagation.h
@@ -1,12 +1,18 @@
 #include <vector>
+#include <math.h>
 #include <cstdlib>
 #include <ctime>
+
+using namespace std;
 
 class Backpropagation
 {
 	// 2 Capas x 2 Neuronas por capas x 3 pesos
 	Backpropagation();
 	float weights[2][2][3];
+	double dot_product(vector<float> inputCase,float *w);
+	double sigma(double x) ;
+	double devSigma(double x);
 	void train(vector< vector<float> >, float);
 	void test(vector< vector<float> >);
 };
