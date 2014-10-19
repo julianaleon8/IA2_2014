e59f4b20e6343d9e320922299a1ac649b123a194
troikm <karentroiano2009@gmail.com>
2014-10-19 13:42:45 -0430

    32-bits? (=

--- pruebaLocal/dataReader.h
+++ pruebaLocal/dataReader.h
@@ -5,13 +5,14 @@
 #include <vector>
 #include <stdlib.h>
 #define NUM_CASES 4
+
 class DataReader {
+	
 public:
 	double patternX[NUM_CASES];
 	double patternY[NUM_CASES];
 
 	double target[NUM_CASES];
-
 
 
 public:
--- pruebaLocal/main.cpp
+++ pruebaLocal/main.cpp
@@ -8,13 +8,15 @@
 
 int main ()
 {
-
 	srand (static_cast <unsigned> (time(0)));
+	
 	DataReader a;
 	a.DataReader::loadData();
 	std::cout << a.target[1];
+	
 	NeuralNetwork b = NeuralNetwork(2, 2, 1);
 	b.train_network(a);
+	
 	double acc = b.test(a);
 	std::cout << acc << " test"<< std::endl;
 }
