745b4d892f4381e3d55263699f9b4fdc9d674ce3
troikm <karentroiano2009@gmail.com>
2014-10-21 00:00:33 -0430

    creo que el case_generator ya coopera

--- Proyecto1-2/case_generator.py
+++ Proyecto1-2/case_generator.py
@@ -19,13 +19,23 @@ if num_cases == None:
 
 print(num_cases)
 
+limit = int(num_cases) / 2
+not_in = 0
+_in = 0
+
 myfile = open('my_data' + num_cases, 'w')
 
 for _ in range(int(num_cases)):
 	x = random.uniform(0.0, 20.0)
 	y = random.uniform(0.0, 20.0)
 	res = isInCircle(x, y)
-
-	myfile.write(str(x) + ' ' + str(y) + ' ' + str(res) + '\n')
+	
+	if (res == -1 and not_in < limit):
+		not_in = not_in + 1
+		myfile.write(str(x) + ' ' + str(y) + ' ' + str(res) + '\n')
+	elif (res == 1 and _in < limit):
+		_in = _in + 1
+		myfile.write(str(x) + ' ' + str(y) + ' ' + str(res) + '\n')
+		
 
 myfile.close()
